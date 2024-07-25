5be4a8164b51f155a80b29d22ff1cde6924508da
washington <washington@meuprecatorio.com.br>
2024-07-25 18:30:32 -0300

    More changes

--- src/app/servicos.tsx
+++ src/app/servicos.tsx
@@ -3,16 +3,22 @@
 import Image from "next/image";
 import { Typography, Card, CardBody } from "@material-tailwind/react";
 
-const CardSection = ({ isImageFirst, imageSrc, title, description }) => (
+const CardSection = ({ isImageFirst, imageSrc, title, description, index }) => (
   <CardBody className="col-span-full gap-1 place-items-center overflow-visible grid grid-cols-1 lg:grid-cols-4">
     {isImageFirst && (
-      <div className="h-[400px] xl:w-[600px] flex items-center overflow-hidden rounded-xl justify-right col-span-2">
+      <div
+        className={`h-[400px] xl:w-[624px] flex items-start overflow-hidden rounded-xl justify-right col-span-2 ${
+          index === 0 ? "mt-4" : ""
+        }`}
+      >
         <Image
-          width={300}
-          height={300}
+          width={624}
+          height={400}
           src={imageSrc}
           alt={title}
-          className="w-full h-full scale-110 object-cover"
+          className={`w-full h-full scale-110 object-cover transition-transform duration-300 hover:scale-125 ${
+            index === 0 ? "object-top" : ""
+          }`}
         />
       </div>
     )}
@@ -25,24 +31,24 @@ const CardSection = ({ isImageFirst, imageSrc, title, description }) => (
       </Typography>
     </div>
     {!isImageFirst && (
-      <div className="h-[400px] xl:w-[600px] flex items-center overflow-hidden rounded-xl justify-center col-span-2">
+      <div className="h-[400px] xl:w-[624px] flex items-center overflow-hidden rounded-xl justify-center col-span-2">
         <Image
-          width={300}
-          height={300}
+          width={624}
+          height={400}
           src={imageSrc}
           alt={title}
-          className="w-full h-full scale-110 object-cover"
+          className="w-full h-full scale-110 object-cover transition-transform duration-300 hover:scale-125"
         />
       </div>
     )}
   </CardBody>
 );
 
-function TESTIMONIAL() {
+function SERVICES() {
   const sections = [
     {
       isImageFirst: true,
-      imageSrc: "/image/imagem1.jpg",
+      imageSrc: "/image/img1.jpg",
       title: "LAVAGEM DETALHADA",
       description:
         "Nossa lavagem detalhada se destaca pela atenção meticulosa aos detalhes, garantindo que cada canto do seu veículo receba o cuidado necessário para um resultado final impecável. Utilizamos técnicas avançadas e produtos de alta qualidade para proporcionar uma limpeza profunda.",
@@ -57,7 +63,7 @@ function TESTIMONIAL() {
     {
       isImageFirst: true,
       imageSrc: "/image/polimento.jpg",
-      title: "POLIMENTO",
+      title: "POLIMENTO DE FAROIS",
       description:
         "O polimento é realizado utilizando técnicas avançadas que visam a remoção de imperfeições e a restauração do brilho original da pintura. Com nossa abordagem especializada, seu veículo terá uma aparência renovada, com um acabamento brilhante e livre de riscos e marcas indesejadas.",
     },
@@ -68,7 +74,7 @@ function TESTIMONIAL() {
       <div id="servicos" className="container mx-auto">
         <Card color="transparent" shadow={false}>
           {sections.map((section, index) => (
-            <CardSection key={index} {...section} />
+            <CardSection key={index} index={index} {...section} />
           ))}
         </Card>
       </div>
@@ -76,4 +82,4 @@ function TESTIMONIAL() {
   );
 }
 
-export default TESTIMONIAL;
+export default SERVICES;
