2614b74c8c0a769fe97aed22b8ee497b0f5033d4
washington <washington@meuprecatorio.com.br>
2024-07-25 18:41:48 -0300

    add more info

--- src/app/servicos.tsx
+++ src/app/servicos.tsx
@@ -1,50 +1,66 @@
 "use client";
 
 import Image from "next/image";
+import PropTypes from "prop-types";
 import { Typography, Card, CardBody } from "@material-tailwind/react";
 
-const CardSection = ({ isImageFirst, imageSrc, title, description, index }) => (
-  <CardBody className="col-span-full gap-1 place-items-center overflow-visible grid grid-cols-1 lg:grid-cols-4">
-    {isImageFirst && (
-      <div
-        className={`h-[400px] xl:w-[624px] flex items-start overflow-hidden rounded-xl justify-right col-span-2 ${
-          index === 0 ? "mt-4" : ""
-        }`}
-      >
-        <Image
-          width={624}
-          height={400}
-          src={imageSrc}
-          alt={title}
-          className={`w-full h-full scale-110 object-cover transition-transform duration-300 hover:scale-125 ${
-            index === 0 ? "object-top" : ""
-          }`}
-        />
-      </div>
-    )}
-    <div className="col-span-2 w-full text-center">
-      <Typography variant="h2" color="blue-gray" className="mb-4 font-bold">
-        {title}
-      </Typography>
-      <Typography className="mb-1 w-full font-large !text-gray-500">
-        {description}
-      </Typography>
-    </div>
-    {!isImageFirst && (
-      <div className="h-[400px] xl:w-[624px] flex items-center overflow-hidden rounded-xl justify-center col-span-2">
-        <Image
-          width={624}
-          height={400}
-          src={imageSrc}
-          alt={title}
-          className="w-full h-full scale-110 object-cover transition-transform duration-300 hover:scale-125"
-        />
-      </div>
-    )}
-  </CardBody>
-);
+const CardSection = ({ isImageFirst, imageSrc, title, description, index }) => {
+  // Determine if the image should have object-top class
+  const isObjectTop = index === 0 || imageSrc === "/image/polimentoLata.jpg";
 
-function SERVICES() {
+  return (
+    <CardBody className="col-span-full gap-1 place-items-center overflow-visible grid grid-cols-1 lg:grid-cols-4">
+      {isImageFirst && (
+        <div
+          className={`h-[400px] xl:w-[624px] flex items-start overflow-hidden rounded-xl justify-right col-span-2 ${
+            index === 0 ? "mt-4" : ""
+          }`}
+        >
+          <Image
+            width={624}
+            height={400}
+            src={imageSrc}
+            alt={title}
+            className={`w-full h-full scale-80 object-cover transition-transform duration-300 hover:scale-125 ${
+              isObjectTop ? "object-top" : ""
+            }`}
+          />
+        </div>
+      )}
+      <div className="col-span-2 w-full text-center">
+        <Typography variant="h2" color="blue-gray" className="mb-4 font-bold">
+          {title}
+        </Typography>
+        <Typography className="mb-1 w-full font-large !text-gray-500">
+          {description}
+        </Typography>
+      </div>
+      {!isImageFirst && (
+        <div className="h-[400px] xl:w-[624px] flex items-center overflow-hidden rounded-xl justify-center col-span-2">
+          <Image
+            width={624}
+            height={400}
+            src={imageSrc}
+            alt={title}
+            className={`w-full h-full scale-80 object-cover transition-transform duration-300 hover:scale-125 ${
+              isObjectTop ? "object-top" : ""
+            }`}
+          />
+        </div>
+      )}
+    </CardBody>
+  );
+};
+
+CardSection.propTypes = {
+  isImageFirst: PropTypes.bool.isRequired,
+  imageSrc: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+};
+
+function Services() {
   const sections = [
     {
       isImageFirst: true,
@@ -62,10 +78,17 @@ function SERVICES() {
     },
     {
       isImageFirst: true,
+      imageSrc: "/image/polimentoLata.jpg",
+      title: "POLIMENTO DE LATARIA",
+      description:
+        "O polimento é realizado utilizando formas profissionais que visam a remoção de imperfeições e a restauração do brilho original da pintura. Com nossa abordagem especializada, seu veículo terá uma aparência renovada, com um acabamento brilhante e livre de riscos e marcas indesejadas.",
+    },
+    {
+      isImageFirst: false,
       imageSrc: "/image/polimento.jpg",
       title: "POLIMENTO DE FAROIS",
       description:
-        "O polimento é realizado utilizando técnicas avançadas que visam a remoção de imperfeições e a restauração do brilho original da pintura. Com nossa abordagem especializada, seu veículo terá uma aparência renovada, com um acabamento brilhante e livre de riscos e marcas indesejadas.",
+        "O polimento de faróis é um serviço essencial para restaurar a clareza e a eficácia dos faróis do seu veículo. Com o tempo, os faróis podem ficar opacos e amarelados devido à exposição a elementos externos e à oxidação. Nosso processo utiliza técnicas e produtos especializados para remover camadas de sujeira e desgaste, restaurando a transparência e a intensidade da luz.",
     },
   ];
 
@@ -82,4 +105,4 @@ function SERVICES() {
   );
 }
 
-export default SERVICES;
+export default Services;
