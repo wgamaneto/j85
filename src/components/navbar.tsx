3c1b17e5638f3d146cf09591773b30c650b7e02a
washington <washington@meuprecatorio.com.br>
2024-08-06 11:54:05 -0300

    fixing

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -72,11 +72,11 @@ export function Navbar() {
 
   return (
     <div className="px-4 lg:px-10 sticky top-4 z-50">
-      <div className="container mx-auto">
+      <div className="container mx-auto relative">
         <MTNavbar
           blurred
           color="white"
-          className="relative border-0 pr-3 py-3 pl-6"
+          className="relative border-0 pr-3 py-3 pl-6 z-10"
         >
           <div className="flex items-center justify-between h-[60px]">
             <Image
@@ -84,7 +84,7 @@ export function Navbar() {
               alt="J85 Logo"
               width={120}
               height={120}
-              className="h-[140px] xl:w-[140px] rounded-xl"
+              className="h-[140px] xl:w-[140px] rounded-xl z-20"
             />
             <div className="hidden lg:flex items-center gap-3">
               <Button variant="text" onClick={scrollToTop}>
@@ -105,26 +105,31 @@ export function Navbar() {
               <Button variant="text" onClick={scrollToBottom}>
                 Localização
               </Button>
+              <div className="flex gap-6 ml-8">
+                <NavItem href="https://www.facebook.com/profile.php?id=61550896696717&mibextid=ZbWKwL">
+                  <Image src={facebook} alt="Facebook" width={30} height={30} />
+                </NavItem>
+                <NavItem href="https://www.instagram.com/garagem_j85?igsh=MXM0azVwemFuY2c4NA==">
+                  <Image
+                    src={instagram}
+                    alt="Instagram"
+                    width={30}
+                    height={30}
+                  />
+                </NavItem>
+                <NavItem href="https://www.tiktok.com/@garagem_j85?_t=8oQVWPaavm1&_r=1">
+                  <Image src={tiktok} alt="TikTok" width={30} height={30} />
+                </NavItem>
+                <NavItem href="https://wa.me/message/47DEWPGQMSAWH1">
+                  <Image src={whatsapp} alt="WhatsApp" width={30} height={30} />
+                </NavItem>
+              </div>
             </div>
-            <ul className="hidden lg:flex ml-8 justify-left gap-6">
-              <NavItem href="https://www.facebook.com/profile.php?id=61550896696717&mibextid=ZbWKwL">
-                <Image src={facebook} alt="Facebook" width={30} height={30} />
-              </NavItem>
-              <NavItem href="https://www.instagram.com/garagem_j85?igsh=MXM0azVwemFuY2c4NA==">
-                <Image src={instagram} alt="Instagram" width={30} height={30} />
-              </NavItem>
-              <NavItem href="https://www.tiktok.com/@garagem_j85?_t=8oQVWPaavm1&_r=1">
-                <Image src={tiktok} alt="TikTok" width={30} height={30} />
-              </NavItem>
-              <NavItem href="https://wa.me/message/47DEWPGQMSAWH1">
-                <Image src={whatsapp} alt="WhatsApp" width={30} height={30} />
-              </NavItem>
-            </ul>
             <IconButton
               variant="text"
               color="gray"
               onClick={handleOpen}
-              className="lg:hidden"
+              className="lg:hidden z-30"
               aria-label="Toggle navigation"
             >
               {open ? (
@@ -135,7 +140,7 @@ export function Navbar() {
             </IconButton>
           </div>
           {open && (
-            <div className="lg:hidden absolute top-16 left-0 w-full bg-white shadow-lg p-4">
+            <div className="lg:hidden absolute top-16 left-0 w-full bg-white shadow-lg p-4 z-0">
               <ul className="flex flex-col items-center gap-4">
                 <Button onClick={() => scrollToTop()}>Inicio</Button>
                 <Button onClick={() => scrollToElement("servicos")}>
